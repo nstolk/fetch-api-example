db8fe88451eadf9497b09afbd91406589ada8c43
nstolk <nick_stolk9@hotmail.com>
2019-09-12 10:03:12 +0200

    Initial commit

---
+++ js/postMethods.js
@@ -0,0 +1,17 @@
+function addPost(e) {
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let body = document.getElementById('body').value;
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ title: title, body: body })
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+}
